ab14b5313b313afced3adfe7d434e9f40c8b9997
Oscar <oscar.hui@live.com>
2024-01-02 16:15:59 +0000

    fix: folder name change

---
+++ dataclean/merge_hpm_to_pp.py
@@ -0,0 +1,12 @@
+import pandas as pd
+DATA_DIR = "./data"
+
+
+DATA_2018 = pd.read_csv(DATA_DIR + "/pp-2018.csv")
+DATA_2019 = pd.read_csv(DATA_DIR + "/pp-2019.csv")
+DATA_2020 = pd.read_csv(DATA_DIR + "/pp-2020.csv")
+DATA_2021 = pd.read_csv(DATA_DIR + "/pp-2021.csv")
+
+
+
+
