c21117b6d04fadc6e421b4212febc45a569cceac
Oscar <oscar.hui@live.com>
2024-01-23 13:54:12 +0000

    feat: implement merged data

--- dataclean/merge_hpm_to_pp.py
+++ dataclean/merge_hpm_to_pp.py
@@ -1,12 +1,100 @@
 import pandas as pd
 DATA_DIR = "./data"
+HOUSE_SIZE_DIR = "./data/house_size_by_borough"
 
 
+DATA_1995 = pd.read_csv(DATA_DIR + "/pp-1995.csv")
+DATA_1996 = pd.read_csv(DATA_DIR + "/pp-1996.csv")
+DATA_1997 = pd.read_csv(DATA_DIR + "/pp-1997.csv")
+DATA_1998 = pd.read_csv(DATA_DIR + "/pp-1998.csv")
+DATA_1999 = pd.read_csv(DATA_DIR + "/pp-1999.csv")
+DATA_2000 = pd.read_csv(DATA_DIR + "/pp-2000.csv")
+DATA_2001 = pd.read_csv(DATA_DIR + "/pp-2001.csv")
+DATA_2002 = pd.read_csv(DATA_DIR + "/pp-2002.csv")
+DATA_2003 = pd.read_csv(DATA_DIR + "/pp-2003.csv")
+DATA_2004 = pd.read_csv(DATA_DIR + "/pp-2004.csv")
+DATA_2005 = pd.read_csv(DATA_DIR + "/pp-2005.csv")
+DATA_2006 = pd.read_csv(DATA_DIR + "/pp-2006.csv")
+DATA_2007 = pd.read_csv(DATA_DIR + "/pp-2007.csv")
+DATA_2008 = pd.read_csv(DATA_DIR + "/pp-2008.csv")
+DATA_2009 = pd.read_csv(DATA_DIR + "/pp-2009.csv")
+DATA_2010 = pd.read_csv(DATA_DIR + "/pp-2010.csv")
+DATA_2011 = pd.read_csv(DATA_DIR + "/pp-2011.csv")
+DATA_2012 = pd.read_csv(DATA_DIR + "/pp-2012.csv")
+DATA_2013 = pd.read_csv(DATA_DIR + "/pp-2013.csv")
+DATA_2014 = pd.read_csv(DATA_DIR + "/pp-2014.csv")
+DATA_2015 = pd.read_csv(DATA_DIR + "/pp-2015.csv")
+DATA_2016 = pd.read_csv(DATA_DIR + "/pp-2016.csv")
+DATA_2017 = pd.read_csv(DATA_DIR + "/pp-2017.csv")
 DATA_2018 = pd.read_csv(DATA_DIR + "/pp-2018.csv")
 DATA_2019 = pd.read_csv(DATA_DIR + "/pp-2019.csv")
 DATA_2020 = pd.read_csv(DATA_DIR + "/pp-2020.csv")
 DATA_2021 = pd.read_csv(DATA_DIR + "/pp-2021.csv")
 
+Barking_and_Dagenham = pd.read_csv(HOUSE_SIZE_DIR + "/Barking_and_Dagenham_HouseSpec.csv")
+Barnet = pd.read_csv(HOUSE_SIZE_DIR + "/Barnet_HouseSpec.csv")
+Bexley = pd.read_csv(HOUSE_SIZE_DIR + "/Bexley_HouseSpec.csv")
+Brent = pd.read_csv(HOUSE_SIZE_DIR + "/Brent_HouseSpec.csv")
+Bromley = pd.read_csv(HOUSE_SIZE_DIR + "/Bromley_HouseSpec.csv")
+Camden = pd.read_csv(HOUSE_SIZE_DIR + "/Camden_HouseSpec.csv")
+Croydon = pd.read_csv(HOUSE_SIZE_DIR + "/Croydon_HouseSpec.csv")
+Ealing = pd.read_csv(HOUSE_SIZE_DIR + "/Ealing_HouseSpec.csv")
+Enfield = pd.read_csv(HOUSE_SIZE_DIR + "/Enfield_HouseSpec.csv")
+Greenwich = pd.read_csv(HOUSE_SIZE_DIR + "/Greenwich_HouseSpec.csv")
+Hackney = pd.read_csv(HOUSE_SIZE_DIR + "/Hackney_HouseSpec.csv")
+Hammersmith_and_Fulham = pd.read_csv(HOUSE_SIZE_DIR + "/Hammersmith_and_Fulham_HouseSpec.csv")
+Haringey = pd.read_csv(HOUSE_SIZE_DIR + "/Haringey_HouseSpec.csv")
+Harrow = pd.read_csv(HOUSE_SIZE_DIR + "/Harrow_HouseSpec.csv")
+Havering = pd.read_csv(HOUSE_SIZE_DIR + "/Havering_HouseSpec.csv")
+Hillingdon = pd.read_csv(HOUSE_SIZE_DIR + "/Hillingdon_HouseSpec.csv")
+Hounslow = pd.read_csv(HOUSE_SIZE_DIR + "/Hounslow_HouseSpec.csv")
+Islington = pd.read_csv(HOUSE_SIZE_DIR + "/Islington_HouseSpec.csv")
+Kensington_and_Chelsea = pd.read_csv(HOUSE_SIZE_DIR + "/Kensington_and_Chelsea_HouseSpec.csv")
+Kingston_upon_Thames = pd.read_csv(HOUSE_SIZE_DIR + "/Kingston_upon_Thames_HouseSpec.csv")
+Lambeth = pd.read_csv(HOUSE_SIZE_DIR + "/Lambeth_HouseSpec.csv")
+Lewisham = pd.read_csv(HOUSE_SIZE_DIR + "/Lewisham_HouseSpec.csv")
+Merton = pd.read_csv(HOUSE_SIZE_DIR + "/Merton_HouseSpec.csv")
+Newham = pd.read_csv(HOUSE_SIZE_DIR + "/Newham_HouseSpec.csv")
+Redbridge = pd.read_csv(HOUSE_SIZE_DIR + "/Redbridge_HouseSpec.csv")
+Richmond_upon_Thames = pd.read_csv(HOUSE_SIZE_DIR + "/Richmond_upon_Thames_HouseSpec.csv")
+Southwark = pd.read_csv(HOUSE_SIZE_DIR + "/Southwark_HouseSpec.csv")
+Sutton = pd.read_csv(HOUSE_SIZE_DIR + "/Sutton_HouseSpec.csv")
+Tower_Hamlets = pd.read_csv(HOUSE_SIZE_DIR + "/Tower_Hamlets_HouseSpec.csv")
+Waltham_Forest = pd.read_csv(HOUSE_SIZE_DIR + "/Waltham_Forest_HouseSpec.csv")
+Wandsworth = pd.read_csv(HOUSE_SIZE_DIR + "/Wandsworth_HouseSpec.csv")
+Westminster = pd.read_csv(HOUSE_SIZE_DIR + "/Westminster_HouseSpec.csv")
+City_of_London = pd.read_csv(HOUSE_SIZE_DIR + "/City_of_London_HouseSpec.csv")
 
+boroughList = [Barking_and_Dagenham, Barnet, Bexley, Brent, Bromley, 
+               Camden, Croydon, Ealing, Enfield, Greenwich, Hackney, 
+               Hammersmith_and_Fulham, Haringey, Harrow, Havering, Hillingdon, 
+               Hounslow, Islington, Kensington_and_Chelsea, Kingston_upon_Thames, 
+               Lambeth, Lewisham, Merton, Newham, Redbridge, Richmond_upon_Thames, 
+               Southwark, Sutton, Tower_Hamlets, Waltham_Forest, Wandsworth, Westminster, 
+               City_of_London]
 
+dataList = [DATA_1995, DATA_1996, DATA_1997, DATA_1998, DATA_1999, DATA_2000, DATA_2001,
+            DATA_2002, DATA_2003, DATA_2004, DATA_2005, DATA_2006, DATA_2007, DATA_2008,
+            DATA_2009, DATA_2010, DATA_2011, DATA_2012, DATA_2013, DATA_2014, DATA_2015,
+            DATA_2016, DATA_2017, DATA_2018, DATA_2019, DATA_2020, DATA_2021]
 
+all_boroughs = pd.concat(boroughList, ignore_index=True)
+
+all_years_data = pd.DataFrame()
+for year, dataLoop in zip(range(1995, 2022), dataList):
+    dataLoop['Year'] = year
+    all_years_data = pd.concat([all_years_data, dataLoop], ignore_index=True)
+
+merged_data = pd.merge(all_years_data, all_boroughs, left_on='ID', right_on='transactionid', how='left')
+
+merged_data['PricePer'] = merged_data['priceper']
+merged_data['NumOfRms'] = merged_data['numberrooms']
+merged_data['HouseSize'] = merged_data['tfarea']
+merged_data['EnergyEfficiency'] = merged_data['CURRENT_ENERGY_EFFICIENCY']
+merged_data['BuildDate'] = merged_data['CONSTRUCTION_AGE_BAND']
+
+for year in range(1995, 2022):
+    print(f"Saved {year} data")
+    year_data = merged_data[merged_data['Year'] == year]
+    year_data.drop(columns=['Year']).to_csv(DATA_DIR + f"/pp-sorted{year}.csv", index=False)
+    
--- dataclean/ppd_clean.py
+++ dataclean/ppd_clean.py
@@ -1,142 +1,71 @@
 import pandas as pd
 DATA_DIR = "./data"
 
-DATA_1995_1_CSV = DATA_DIR + "/pp-1995-part1.csv"
-DATA_1995_2_CSV = DATA_DIR + "/pp-1995-part2.csv"
-DATA_1996_1_CSV = DATA_DIR + "/pp-1996-part1.csv"
-DATA_1996_2_CSV = DATA_DIR + "/pp-1996-part2.csv"
-DATA_1997_1_CSV = DATA_DIR + "/pp-1997-part1.csv"
-DATA_1997_2_CSV = DATA_DIR + "/pp-1997-part2.csv"
-DATA_1998_1_CSV = DATA_DIR + "/pp-1998-part1.csv"
-DATA_1998_2_CSV = DATA_DIR + "/pp-1998-part2.csv"
-DATA_1999_1_CSV = DATA_DIR + "/pp-1999-part1.csv"
-DATA_1999_2_CSV = DATA_DIR + "/pp-1999-part2.csv"
-DATA_2000_1_CSV = DATA_DIR + "/pp-2000-part1.csv"
-DATA_2000_2_CSV = DATA_DIR + "/pp-2000-part2.csv"
-DATA_2001_1_CSV = DATA_DIR + "/pp-2001-part1.csv"
-DATA_2001_2_CSV = DATA_DIR + "/pp-2001-part2.csv"
-DATA_2002_1_CSV = DATA_DIR + "/pp-2002-part1.csv"
-DATA_2002_2_CSV = DATA_DIR + "/pp-2002-part2.csv"
-DATA_2003_1_CSV = DATA_DIR + "/pp-2003-part1.csv"
-DATA_2003_2_CSV = DATA_DIR + "/pp-2003-part2.csv"
-DATA_2004_1_CSV = DATA_DIR + "/pp-2004-part1.csv"
-DATA_2004_2_CSV = DATA_DIR + "/pp-2004-part2.csv"
-DATA_2005_1_CSV = DATA_DIR + "/pp-2005-part1.csv"
-DATA_2005_2_CSV = DATA_DIR + "/pp-2005-part2.csv"
-DATA_2006_1_CSV = DATA_DIR + "/pp-2006-part1.csv"
-DATA_2006_2_CSV = DATA_DIR + "/pp-2006-part2.csv"
-DATA_2007_1_CSV = DATA_DIR + "/pp-2007-part1.csv"
-DATA_2007_2_CSV = DATA_DIR + "/pp-2007-part2.csv"
+DATA_1995_CSV = DATA_DIR + "/pp-1995.csv"
+DATA_1996_CSV = DATA_DIR + "/pp-1996.csv"
+DATA_1997_CSV = DATA_DIR + "/pp-1997.csv"
+DATA_1998_CSV = DATA_DIR + "/pp-1998.csv"
+DATA_1999_CSV = DATA_DIR + "/pp-1999.csv"
+DATA_2000_CSV = DATA_DIR + "/pp-2000.csv"
+DATA_2001_CSV = DATA_DIR + "/pp-2001.csv"
+DATA_2002_CSV = DATA_DIR + "/pp-2002.csv"
+DATA_2003_CSV = DATA_DIR + "/pp-2003.csv"
+DATA_2004_CSV = DATA_DIR + "/pp-2004.csv"
+DATA_2005_CSV = DATA_DIR + "/pp-2005.csv"
+DATA_2006_CSV = DATA_DIR + "/pp-2006.csv"
+DATA_2007_CSV = DATA_DIR + "/pp-2007.csv"
+DATA_2008_CSV = DATA_DIR + "/pp-2008.csv"
+DATA_2009_CSV = DATA_DIR + "/pp-2009.csv"
+DATA_2010_CSV = DATA_DIR + "/pp-2010.csv"
+DATA_2011_CSV = DATA_DIR + "/pp-2011.csv"
+DATA_2012_CSV = DATA_DIR + "/pp-2012.csv"
+DATA_2013_CSV = DATA_DIR + "/pp-2013.csv"
+DATA_2014_CSV = DATA_DIR + "/pp-2014.csv"
+DATA_2015_CSV = DATA_DIR + "/pp-2015.csv"
+DATA_2016_CSV = DATA_DIR + "/pp-2016.csv"
+DATA_2017_CSV = DATA_DIR + "/pp-2017.csv"
+DATA_2018_CSV = DATA_DIR + "/pp-2018.csv"
+DATA_2019_CSV = DATA_DIR + "/pp-2019.csv"
+DATA_2020_CSV = DATA_DIR + "/pp-2020.csv"
+DATA_2021_CSV = DATA_DIR + "/pp-2021.csv"
 
 
 
-DATA_1995_1 = pd.read_csv(DATA_1995_1_CSV)
-DATA_1995_2 = pd.read_csv(DATA_1995_2_CSV)
-DATA_1996_1 = pd.read_csv(DATA_1996_1_CSV)
-DATA_1996_2 = pd.read_csv(DATA_1996_2_CSV)
-DATA_1997_1 = pd.read_csv(DATA_1997_1_CSV)
-DATA_1997_2 = pd.read_csv(DATA_1997_2_CSV)
-DATA_1998_1 = pd.read_csv(DATA_1998_1_CSV)
-DATA_1998_2 = pd.read_csv(DATA_1998_2_CSV)
-DATA_1999_1 = pd.read_csv(DATA_1999_1_CSV)
-DATA_1999_2 = pd.read_csv(DATA_1999_2_CSV)
-DATA_2000_1 = pd.read_csv(DATA_2000_1_CSV)
-DATA_2000_2 = pd.read_csv(DATA_2000_2_CSV)
-DATA_2001_1 = pd.read_csv(DATA_2001_1_CSV)
-DATA_2001_2 = pd.read_csv(DATA_2001_2_CSV)
-DATA_2002_1 = pd.read_csv(DATA_2002_1_CSV)
-DATA_2002_2 = pd.read_csv(DATA_2002_2_CSV)
-DATA_2003_1 = pd.read_csv(DATA_2003_1_CSV)
-DATA_2003_2 = pd.read_csv(DATA_2003_2_CSV)
-DATA_2004_1 = pd.read_csv(DATA_2004_1_CSV)
-DATA_2004_2 = pd.read_csv(DATA_2004_2_CSV)
-DATA_2005_1 = pd.read_csv(DATA_2005_1_CSV)
-DATA_2005_2 = pd.read_csv(DATA_2005_2_CSV)
-DATA_2006_1 = pd.read_csv(DATA_2006_1_CSV)
-DATA_2006_2 = pd.read_csv(DATA_2006_2_CSV)
-DATA_2007_1 = pd.read_csv(DATA_2007_1_CSV)
-DATA_2007_2 = pd.read_csv(DATA_2007_2_CSV)
+DATA_1995 = pd.read_csv(DATA_1995_CSV)
+DATA_1996 = pd.read_csv(DATA_1996_CSV)
+DATA_1997 = pd.read_csv(DATA_1997_CSV)
+DATA_1998 = pd.read_csv(DATA_1998_CSV)
+DATA_1999 = pd.read_csv(DATA_1999_CSV)
+DATA_2000 = pd.read_csv(DATA_2000_CSV)
+DATA_2001 = pd.read_csv(DATA_2001_CSV)
+DATA_2002 = pd.read_csv(DATA_2002_CSV)
+DATA_2003 = pd.read_csv(DATA_2003_CSV)
+DATA_2004 = pd.read_csv(DATA_2004_CSV)
+DATA_2005 = pd.read_csv(DATA_2005_CSV)
+DATA_2006 = pd.read_csv(DATA_2006_CSV)
+DATA_2007 = pd.read_csv(DATA_2007_CSV)
+DATA_2008 = pd.read_csv(DATA_2008_CSV)
+DATA_2009 = pd.read_csv(DATA_2009_CSV)
+DATA_2010 = pd.read_csv(DATA_2010_CSV)
+DATA_2011 = pd.read_csv(DATA_2011_CSV)
+DATA_2012 = pd.read_csv(DATA_2012_CSV)
+DATA_2013 = pd.read_csv(DATA_2013_CSV)
+DATA_2014 = pd.read_csv(DATA_2014_CSV)
+DATA_2015 = pd.read_csv(DATA_2015_CSV)
+DATA_2016 = pd.read_csv(DATA_2016_CSV)
+DATA_2017 = pd.read_csv(DATA_2017_CSV)
+DATA_2018 = pd.read_csv(DATA_2018_CSV)
+DATA_2019 = pd.read_csv(DATA_2019_CSV)
+DATA_2020 = pd.read_csv(DATA_2020_CSV)
+DATA_2021 = pd.read_csv(DATA_2021_CSV)
 
-headers = ["ID","Price","Date","Postcode","Type","Newbuild","Ownership","Street Number","Flat Number","Street Name","Area", "Town", "City", "County", "Unnamed: 14", "Unnamed: 15"]
-DATA_1995_1.columns = headers
-DATA_1995_2.columns = headers
-DATA_1996_1.columns = headers
-DATA_1996_2.columns = headers
-DATA_1997_1.columns = headers
-DATA_1997_2.columns = headers
-DATA_1998_1.columns = headers
-DATA_1998_2.columns = headers
-DATA_1999_1.columns = headers
-DATA_1999_2.columns = headers
-DATA_2000_1.columns = headers
-DATA_2000_2.columns = headers
-DATA_2001_1.columns = headers
-DATA_2001_2.columns = headers
-DATA_2002_1.columns = headers
-DATA_2002_2.columns = headers
-DATA_2003_1.columns = headers
-DATA_2003_2.columns = headers
-DATA_2004_1.columns = headers
-DATA_2004_2.columns = headers
-DATA_2005_1.columns = headers
-DATA_2005_2.columns = headers
-DATA_2006_1.columns = headers
-DATA_2006_2.columns = headers
-DATA_2007_1.columns = headers
-DATA_2007_2.columns = headers
 
-DATA_1995_1 = DATA_1995_1.query("County == 'GREATER LONDON'")
-DATA_1995_2 = DATA_1995_2.query("County == 'GREATER LONDON'")
-DATA_1996_1 = DATA_1996_1.query("County == 'GREATER LONDON'")
-DATA_1996_2 = DATA_1996_2.query("County == 'GREATER LONDON'")
-DATA_1997_1 = DATA_1997_1.query("County == 'GREATER LONDON'")
-DATA_1997_2 = DATA_1997_2.query("County == 'GREATER LONDON'")
-DATA_1998_1 = DATA_1998_1.query("County == 'GREATER LONDON'")
-DATA_1998_2 = DATA_1998_2.query("County == 'GREATER LONDON'")
-DATA_1999_1 = DATA_1999_1.query("County == 'GREATER LONDON'")
-DATA_1999_2 = DATA_1999_2.query("County == 'GREATER LONDON'")
-DATA_2000_1 = DATA_2000_1.query("County == 'GREATER LONDON'")
-DATA_2000_2 = DATA_2000_2.query("County == 'GREATER LONDON'")
-DATA_2001_1 = DATA_2001_1.query("County == 'GREATER LONDON'")
-DATA_2001_2 = DATA_2001_2.query("County == 'GREATER LONDON'")
-DATA_2002_1 = DATA_2002_1.query("County == 'GREATER LONDON'")
-DATA_2002_2 = DATA_2002_2.query("County == 'GREATER LONDON'")
-DATA_2003_1 = DATA_2003_1.query("County == 'GREATER LONDON'")
-DATA_2003_2 = DATA_2003_2.query("County == 'GREATER LONDON'")
-DATA_2004_1 = DATA_2004_1.query("County == 'GREATER LONDON'")
-DATA_2004_2 = DATA_2004_2.query("County == 'GREATER LONDON'")
-DATA_2005_1 = DATA_2005_1.query("County == 'GREATER LONDON'")
-DATA_2005_2 = DATA_2005_2.query("County == 'GREATER LONDON'")
-DATA_2006_1 = DATA_2006_1.query("County == 'GREATER LONDON'")
-DATA_2006_2 = DATA_2006_2.query("County == 'GREATER LONDON'")
-DATA_2007_1 = DATA_2007_1.query("County == 'GREATER LONDON'")
-DATA_2007_2 = DATA_2007_2.query("County == 'GREATER LONDON'")
 
-DATA_1995 = pd.concat([DATA_1995_1, DATA_1995_2])
-DATA_1996 = pd.concat([DATA_1996_1, DATA_1996_2])
-DATA_1997 = pd.concat([DATA_1997_1, DATA_1997_2])
-DATA_1998 = pd.concat([DATA_1998_1, DATA_1998_2])
-DATA_1999 = pd.concat([DATA_1999_1, DATA_1999_2])
-DATA_2000 = pd.concat([DATA_2000_1, DATA_2000_2])
-DATA_2001 = pd.concat([DATA_2001_1, DATA_2001_2])
-DATA_2002 = pd.concat([DATA_2002_1, DATA_2002_2])
-DATA_2003 = pd.concat([DATA_2003_1, DATA_2003_2])
-DATA_2004 = pd.concat([DATA_2004_1, DATA_2004_2])
-DATA_2005 = pd.concat([DATA_2005_1, DATA_2005_2])
-DATA_2006 = pd.concat([DATA_2006_1, DATA_2006_2])
-DATA_2007 = pd.concat([DATA_2007_1, DATA_2007_2])
+DATA_ALL = pd.concat([DATA_1995, DATA_1996, DATA_1997, DATA_1998, DATA_1999, 
+                  DATA_2000, DATA_2001, DATA_2002, DATA_2003, DATA_2004, 
+                  DATA_2005, DATA_2006, DATA_2007, DATA_2008, DATA_2009, 
+                  DATA_2010, DATA_2011, DATA_2012, DATA_2013, DATA_2014, 
+                  DATA_2015, DATA_2016, DATA_2017, DATA_2018, DATA_2019, 
+                  DATA_2020, DATA_2021], ignore_index=True)
 
-DATA_1995.to_csv(DATA_DIR + "/pp-1995.csv", index=False)
-DATA_1996.to_csv(DATA_DIR + "/pp-1996.csv", index=False)
-DATA_1997.to_csv(DATA_DIR + "/pp-1997.csv", index=False)
-DATA_1998.to_csv(DATA_DIR + "/pp-1998.csv", index=False)
-DATA_1999.to_csv(DATA_DIR + "/pp-1999.csv", index=False)
-DATA_2000.to_csv(DATA_DIR + "/pp-2000.csv", index=False)
-DATA_2001.to_csv(DATA_DIR + "/pp-2001.csv", index=False)
-DATA_2002.to_csv(DATA_DIR + "/pp-2002.csv", index=False)
-DATA_2003.to_csv(DATA_DIR + "/pp-2003.csv", index=False)
-DATA_2004.to_csv(DATA_DIR + "/pp-2004.csv", index=False)
-DATA_2005.to_csv(DATA_DIR + "/pp-2005.csv", index=False)
-DATA_2006.to_csv(DATA_DIR + "/pp-2006.csv", index=False)
-DATA_2007.to_csv(DATA_DIR + "/pp-2007.csv", index=False)
-
+DATA_ALL.to_csv(DATA_DIR + "/pp-all.csv", index=False)
